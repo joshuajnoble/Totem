1ce88e6fecdfb7d2dc3f0f2468ebedbda110a70d
Joshua Noble <jnoble@teague.com>
2015-04-23 15:16:13 -0700

    big updates

--- spoutSender/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "ofMain.h"
-#include "ofApp.h"
-
-//========================================================================
-int main()
-{
-	ofSetupOpenGL(960, 720, OF_WINDOW);
-	ofRunApp(new ofApp());
-}
